45b218cb63a3eb90cf166230bad451dcf8237164
ZIPMERIEN <viktori.iaropovetska@gmail.com>
2017-12-20 22:00:54 +0200

    [l10] Polimorphism

---
+++ src/main/java/com/kitcenter/app/classwork/lesson10/Teacher.java
@@ -0,0 +1,9 @@
+package com.kitcenter.app.classwork.lesson10;
+
+public class Teacher extends Person {
+
+    @Override
+    public void averageAge() {
+        System.out.println("Average teacher\'s age is 37");
+    }
+}
